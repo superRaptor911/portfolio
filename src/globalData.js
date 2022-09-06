4a4b1656c7aa88b216ff56854770c51a4951d675
superRaptor911 <raptor.inc2018@gmail.com>
2022-09-06 15:50:40 +0530

    Created Global data

---
+++ src/globalData.js
@@ -0,0 +1,10 @@
+export const GlobalData = {
+  contact: {
+    email: 'raptor.inc2018@gmail.com',
+    phone: '+918505908858',
+  },
+  social: {
+    github: 'https://github.com/superRaptor911/',
+    linkedin: 'https://www.linkedin.com/in/aditya-aravind-718843202/',
+  },
+};
